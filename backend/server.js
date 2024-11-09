fd045b40a158b5f2d91f4b7dbfe7a87d981acb98
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-11-09 15:16:14 +0530

    add configs for azure

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const productRoutes = require("./routes/productRoutes");
-const userRoutes = require('./routes/userRoutes');
-const cartRoutes = require('./routes/cartRoutes');
+const userRoutes = require("./routes/userRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 
 const app = express();
 
@@ -22,17 +22,27 @@ mongoose
     console.log("Error connecting to MongoDB=", error);
   });
 
-app
-  .get("/", (req, res) => {
-    res.status(200).json("API is running.");
-  });
+app.get("/", (req, res) => {
+  res.status(200).json("API is running.");
+});
 
-app.use('/api/users',userRoutes);
-app.use('/api/products',productRoutes);
-app.use('/api/cart',cartRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 PORT = 5000;
 
-app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}`);
-})
+const path = require("path");
+
+// Serve static files from the frontend if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
